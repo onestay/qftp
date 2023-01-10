1e3b8640bfa7ce073ce6239a8c9dcbe800a3f651
Marius Meschter <git@onestay.moe>
2023-01-10 17:42:17 +0100

    add a server builder

--- qftp/src/server.rs
+++ qftp/src/server.rs
@@ -4,12 +4,89 @@ use quinn::Endpoint;
 use rustls::ServerConfig;
 use rustls::{Certificate, PrivateKey};
 use std::net::SocketAddr;
+use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tracing::debug;
 
 use crate::connected_client::ConnectedClient;
 use crate::files::FileManager;
+
+#[derive(Debug)]
+pub struct ServerBuilder {
+    server_config: Option<ServerConfig>,
+    listen_addr: Option<SocketAddr>,
+    base_path: Option<PathBuf>,
+    auth_file: Option<PathBuf>,
+}
+
+impl ServerBuilder {
+    /// set a custom [ServerConfig](rustls::ServerConfig)
+    pub fn with_server_config(mut self, config: ServerConfig) -> Self {
+        self.server_config = Some(config);
+
+        self
+    }
+
+    /// set the address to listen on
+    pub fn set_listen_addr(mut self, addr: SocketAddr) -> Self {
+        self.listen_addr = Some(addr);
+
+        self
+    }
+
+    /// set the base path for serving files
+    pub fn set_base_path(mut self, path: PathBuf) -> Self {
+        if !path.is_dir() {
+            panic!("base path has to be a directory");
+        }
+
+        self.base_path = Some(path);
+
+        self
+    }
+
+    /// set the location for the client authentication file
+    pub fn set_auth_file(mut self, auth_file: PathBuf) -> Self {
+        if !auth_file.is_file() {
+            panic!("auth_file has to be a path to a file");
+        }
+
+        self.auth_file = Some(auth_file);
+
+        self
+    }
+
+    /// Creates a new default [ServerConfig](rustls::ServerConfig) with the specified certs.
+    /// If you want to supply your own server config you can use [with_server_config](ServerBuilder::with_server_config)
+    pub fn with_certs(
+        mut self,
+        certs: Vec<Certificate>,
+        private_key: PrivateKey,
+    ) -> Self {
+        let server_config = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(certs, private_key)
+            .expect("Couldn't construct new ServerConfig");
+
+        self.server_config = Some(server_config);
+
+        self
+    }
+
+    pub async fn build(self) -> Result<Server, Error> {
+        let server = Server::new(
+            self.listen_addr.expect("didn't set listen_addr"),
+            self.server_config.expect("didn't set ServerConfig"),
+            self.auth_file.expect("didn't set auth_file"),
+            self.base_path.expect("didn't set base_path"),
+        )
+        .await?;
+
+        Ok(server)
+    }
+}
 
 /// Entrypoint for creating a qftp Server
 #[derive(Debug)]
@@ -20,15 +97,19 @@ pub struct Server {
 }
 
 impl Server {
+    pub fn builder() -> ServerBuilder {
+        ServerBuilder {
+            server_config: None,
+            listen_addr: None,
+            base_path: None,
+            auth_file: None,
+        }
+    }
+
     fn create_endpoint(
         listen_addr: SocketAddr,
-        cert: Certificate,
-        priv_key: PrivateKey,
+        server_config: ServerConfig,
     ) -> Result<Endpoint, Error> {
-        let server_config = ServerConfig::builder()
-            .with_safe_defaults()
-            .with_no_client_auth()
-            .with_single_cert(vec![cert], priv_key)?;
         let server_config =
             quinn::ServerConfig::with_crypto(Arc::new(server_config));
         let server = Endpoint::server(server_config, listen_addr)?;
@@ -44,14 +125,14 @@ impl Server {
     /// * `priv_key` - The private key. Refer to [rustls](rustls::PrivateKey) documentation for the correct format
     pub async fn new(
         listen_addr: SocketAddr,
-        cert: Certificate,
-        priv_key: PrivateKey,
+        server_config: ServerConfig,
+        auth_file: PathBuf,
+        base_path: PathBuf,
     ) -> Result<Self, Error> {
-        let server = Server::create_endpoint(listen_addr, cert, priv_key)?;
-        let auth_storage = FileStorage::new("./auth.json").await?;
+        let server = Server::create_endpoint(listen_addr, server_config)?;
+        let auth_storage = FileStorage::new(auth_file).await?;
         let manager = AuthManager::new(auth_storage);
-        let path = format!("{}/tests/walk_dir", env!("CARGO_MANIFEST_DIR"));
-        let file_manager = FileManager::new(path).unwrap();
+        let file_manager = FileManager::new(base_path).unwrap();
         Ok(Server {
             endpoint: server,
             auth: Arc::new(Mutex::new(manager)),
@@ -74,60 +155,5 @@ impl Server {
             }
         }
     }
-
-    pub async fn process_message(&self) {
-        // recieve the header for the request
-    }
-
-    pub async fn process_list_files(&self) {
-        // collect files create vec, struct and all that stuff
-
-        // 1: open new stream
-
-        // 2: send the ListFileResponseHeader with correct request_id
-
-        // 3: send the ListFileResponse
-    }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::fs;
-
-    fn read_test_certs() -> (Certificate, PrivateKey) {
-        let cert =
-            fs::read("cert/dev.crt.der").expect("Failed to read certificate");
-        let cert = Certificate(cert);
-        let priv_key =
-            fs::read("cert/dev.key.der").expect("Failed to read private key");
-        let priv_key = PrivateKey(priv_key);
-
-        (cert, priv_key)
-    }
-
-    #[tokio::test]
-    async fn test_server_new_valid() {
-        let (cert, priv_key) = read_test_certs();
-        let server = Server::new(
-            "0.0.0.0:0".parse().expect("Failed to parse socket addr"),
-            cert,
-            priv_key,
-        )
-        .await;
-        assert!(server.is_ok());
-    }
-
-    #[tokio::test]
-    async fn test_server_invalid_priv_key_cert() {
-        let cert = Certificate(vec![0]);
-        let priv_key = PrivateKey(vec![0]);
-        let server = Server::new(
-            "0.0.0.0:0".parse().expect("Failed to parse socket addr"),
-            cert,
-            priv_key,
-        )
-        .await;
-        assert!(server.is_err());
-    }
-}
--- qftp/tests/server_client_connection_test.rs
+++ qftp/tests/server_client_connection_test.rs
@@ -1,43 +1,54 @@
-use rustls::{Certificate, PrivateKey};
-use std::fs;
-
-fn read_test_certs() -> (Certificate, PrivateKey) {
-    let cert = fs::read("cert/dev.crt.der").expect("Failed to read certificate");
-    let cert = Certificate(cert);
-    let priv_key =
-        fs::read("cert/dev.key.der").expect("Failed to read private key");
-    let priv_key = PrivateKey(priv_key);
-
-    (cert, priv_key)
-}
-
 #[cfg(test)]
 mod test {
-    use qftp::{Client, ClientBuilder, QClientConfig, Server};
+    use qftp::{Client, QClientConfig, Server};
+    use rustls::{Certificate, PrivateKey};
+    use std::{fs, path::PathBuf, str::FromStr};
     use tracing::Level;
     use tracing_subscriber::filter::EnvFilter;
+    fn read_test_certs() -> (Certificate, PrivateKey) {
+        let cert =
+            fs::read("cert/dev.crt.der").expect("Failed to read certificate");
+        let cert = Certificate(cert);
+        let priv_key =
+            fs::read("cert/dev.key.der").expect("Failed to read private key");
+        let priv_key = PrivateKey(priv_key);
+
+        (cert, priv_key)
+    }
+
+    async fn new_default_server() -> Server {
+        let (cert, priv_key) = read_test_certs();
+        let path = format!("{}/tests/walk_dir", env!("CARGO_MANIFEST_DIR"));
+        let auth_file = format!("{}/tests/auth.json", env!("CARGO_MANIFEST_DIR"));
+        let server = Server::builder()
+            .set_listen_addr("0.0.0.0:2345".parse().unwrap())
+            .set_base_path(PathBuf::from_str(&path).unwrap())
+            .set_auth_file(PathBuf::from_str(&auth_file).unwrap())
+            .with_certs(vec![cert], priv_key)
+            .build()
+            .await
+            .unwrap();
+
+        server
+    }
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn successful_server_client_connection() {
         std::env::set_var("RUST_BACKTRACE", "full");
-        // let env_filter = EnvFilter::try_from_default_env()
-        //     .unwrap_or_else(|_| EnvFilter::new("qftp=trace"));
-        // tracing_subscriber::fmt()
-        //     .with_max_level(Level::TRACE)
-        //     .with_env_filter(env_filter)
-        //     .init();
+        let env_filter = EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| EnvFilter::new("qftp=trace"));
+        tracing_subscriber::fmt()
+            .with_max_level(Level::TRACE)
+            .with_env_filter(env_filter)
+            .init();
 
-        let (cert, priv_key) = super::read_test_certs();
         let server = tokio::spawn(async {
-            let server =
-                Server::new("127.0.0.1:2345".parse().unwrap(), cert, priv_key)
-                    .await
-                    .unwrap();
+            let server = new_default_server().await;
             let mut connected_client = server.accept().await.unwrap();
             connected_client
                 .next_request()
                 .await
-                .expect("next request returnd err");
+                .expect("next request returned err");
             connected_client.shutdown().await.unwrap();
         });
 
@@ -51,8 +62,9 @@ mod test {
                 .await
                 .expect("error constructing the client");
             let result = client.list_files().await.unwrap();
+            client.shutdown().await.unwrap();
             assert_eq!(result.len(), 4);
-            //println!("{result:#?}")
+            println!("{result:#?}")
         });
 
         futures::future::join_all(vec![server, client]).await;
